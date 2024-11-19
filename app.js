bedd2e0e9881ebfa30f5805bbb872386c9a4b7c9
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-11-19 16:55:58 +0800

    fix: clean up directories

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = 3000;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 app.use('/', artists);
 
