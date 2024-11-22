46bcbb99cb8b04cdaf28fcda709aacd22fef9b14
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-11-22 23:51:37 +0800

    feat: add follow and unfollow

--- routes/artists.js
+++ routes/artists.js
@@ -12,55 +12,6 @@ const con = mysql.createConnection({
 });
 con.connect();
 
-// Follows
-router.get('/follows', (req, res) => {
-    const sql = "SELECT id, following_artist_id, followed_artist_id, followed_at FROM follows WHERE unfollowed_at IS NULL";
-
-    con.query(sql, (err, result) => {
-        res.render('follows', {
-            follows: result
-        });
-    });
-});
-
-// Reports
-router.get('/reports', (req, res) => {
-    res.render('reports');
-});
-
-// Follow Artist
-router.get('/follows/follow', (req, res) => {
-    const sql = "SELECT * FROM artists ORDER BY id";
-
-    con.query(sql, (err, result) => {
-        res.render('follows/follow', {
-            action: "follow",
-            artists: result
-        });
-    });
-});
-
-router.post('/follows/follow', (req, res) => {
-    const { followed_artist_id, following_artist_id } = req.body;
-
-    const sql = "INSERT INTO follows (followed_at, unfollowed_at, following_artist_id, followed_artist_id) VALUES (NOW(), NULL, ?, ?)";
-
-    con.query(sql, [followed_artist_id, following_artist_id], (err, result) => {
-        res.redirect('/follows');
-    });
-});
-
-// Unfollow Artist
-router.post('/follows/unfollow/:id', (req, res) => {
-    const id = req.params.id;
-
-    const sql = "UPDATE follows SET unfollowed_at = NOW() WHERE id = ?";
-
-    con.query(sql, [id], (err, result) => {
-        res.redirect('/follows');
-    });
-});
-
 // Artists
 router.get('/artists', (req, res) => {
     const sql = "SELECT id, username FROM artists ORDER BY id";
@@ -73,21 +24,29 @@ router.get('/artists', (req, res) => {
 });
 
 // Create Artist
-router.get('/artists/create-artist', (req, res) => {
-    res.render('artists/create-read-update-artist', {
-        action: "create-artist",
+router.get('/artists/create', (req, res) => {
+    const { username, picture, first_name, last_name, bio } = {
+        username: "",
+        picture: "",
+        first_name: "",
+        last_name: "",
+        bio: ""
+    };
+
+    res.render('artists-form', {
+        action: "create",
+        disabled: false,
         artist: {
             username: "",
             picture: "",
             first_name: "",
             last_name: "",
             bio: ""
-        },
-        readonly: false
+        }
     });
 });
 
-router.post('/artists/create-artist', (req, res) => {
+router.post('/artists/create', (req, res) => {
     const { username, picture, first_name, last_name, bio } = req.body;
 
     const sql = "INSERT INTO artists (username, picture, first_name, last_name, bio) VALUES (?, ?, ?, ?, ?)";
@@ -98,39 +57,38 @@ router.post('/artists/create-artist', (req, res) => {
 });
 
 // Read Artist
-router.get('/artists/read-artist/:id', (req, res) => {
+router.get('/artists/read/:id', (req, res) => {
     const id = req.params.id;
 
     const sql = "SELECT * FROM artists WHERE id = ?";
 
     con.query(sql, [id], (err, result) => {
-        res.render('artists/create-read-update-artist', { 
-            action: "",
-            artist: result[0],
-            readonly: true
+        res.render('artists-form', { 
+            action: "read",
+            disabled: true,
+            artist: result[0]
         });
     });
 });
 
 // Update Artist
-router.get('/artists/update-artist/:id', (req, res) => {
+router.get('/artists/update/:id', (req, res) => {
     const id = req.params.id;
 
     const sql = "SELECT * FROM artists WHERE id = ?";
 
     con.query(sql, [id], (err, result) => {
-        res.render('artists/create-read-update-artist', {
+        res.render('artists-form', {
             action: `update-artist/${id}`,
-            artist: result[0],
-            readonly: false
+            disabled: false,
+            artist: result[0]
         });
     });
 });
 
 router.post('/artists/update-artist/:id', (req, res) => {
-    const id = req.params.id;
-
     const { username, picture, first_name, last_name, bio } = req.body;
+    const id = req.params.id;
 
     const sql = "UPDATE artists SET username = ?, picture = ?, first_name = ?, last_name = ?, bio = ? WHERE id = ?";
 
@@ -140,7 +98,7 @@ router.post('/artists/update-artist/:id', (req, res) => {
 });
 
 // Delete Artist
-router.post('/artists/delete-artist/:id', (req, res) => {
+router.post('/artists/delete/:id', (req, res) => {
     const id = req.params.id;
 
     const sql = "DELETE FROM artists WHERE id = ?";
@@ -148,6 +106,63 @@ router.post('/artists/delete-artist/:id', (req, res) => {
     con.query(sql, [id], (err, result) => {
         res.redirect('/artists');
     });
+});
+
+// Follows
+router.get('/follows', (req, res) => {
+    const selected = req.query.select;
+
+    const sql = "SELECT id, username FROM artists ORDER BY id"
+
+    con.query(sql, (err, result) => {
+        const options = result;
+
+        const sql = "SELECT a.id, a.username FROM artists a JOIN follows f ON a.id = f.followed_artist_id WHERE f.following_artist_id = ? AND f.unfollowed_at IS NULL ORDER BY a.id";
+        
+        con.query(sql, [selected], (err, result) => {
+            const following = result;
+
+            const sql = "SELECT a.id, a.username FROM artists a LEFT JOIN follows f ON a.id = f.followed_artist_id AND f.following_artist_id = ? AND f.unfollowed_at is NULL WHERE f.id IS NULL AND a.id != ? ORDER BY a.id";
+
+            con.query(sql, [selected, selected], (err, result) => {
+                res.render('follows', {
+                    id: selected,
+                    options: options,
+                    following: following,
+                    recommended: result
+                });
+            });
+        });
+    });
+});
+
+// Follow Artist
+router.post('/follows/follow/:following_id/:followed_id', (req, res) => {
+    const following_id = req.params.following_id;
+    const followed_id = req.params.followed_id;
+
+    const sql = "INSERT INTO follows (followed_at, unfollowed_at, following_artist_id, followed_artist_id) VALUES (NOW(), NULL, ?, ?)"
+
+    con.query(sql, [following_id, followed_id], (err, result) => {
+        res.redirect(`/follows/?select=${following_id}`);
+    });
+});
+
+// Unfollow Artist
+router.post('/follows/unfollow/:following_id/:followed_id', (req, res) => {
+    const following_id = req.params.following_id;
+    const followed_id = req.params.followed_id;
+
+    const sql = "UPDATE follows SET unfollowed_at = NOW() WHERE following_artist_id = ? AND followed_artist_id = ? AND unfollowed_at IS NULL";
+
+    con.query(sql, [following_id, followed_id], (err, result) => {
+        res.redirect(`/follows/?select=${following_id}`);
+    });
+});
+
+// Report
+router.get('/reports', (req, res) => {
+    res.render('reports');
 });
 
 // Export
