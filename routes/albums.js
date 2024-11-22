03546351cc41e94fd8cfe338a3020cc0022c8625
Melting4jisoo <stephen_y_co@dlsu.edu.ph>
2024-11-23 05:40:20 +0800

    Feats: likes

--- routes/albums.js
+++ routes/albums.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "[{<kiewbI>}]",
+    password: "Co_Stephen01.",
     database: "music_streaming"
 });
 
@@ -34,7 +34,7 @@ router.get('/albums/create', (req, res) => {
 
     con.query("SELECT * FROM albums", (err, option_ids) => {
         res.render('albums-form', {
-            action: "create-albums",
+            action: "create",
             disabled: false,
             albums: {
                 title: "",
@@ -82,13 +82,14 @@ router.get('/albums/update-album/:id', (req, res) => {
     const id = req.params.id;
 
     const sql = "SELECT * FROM albums WHERE id = ?";
+
     con.query("SELECT * FROM albums", (err, option_ids) => {
         con.query(sql, [id], (err, result) => {
             res.render('albums-form', {
-                action: `update-album/${id}`,
+                action: `update/${id}`,
                 disabled: false,
                 albums: result[0],
-                readonly: false,
+
                 option_ids: option_ids,
             });
         });
@@ -102,6 +103,7 @@ router.post('/albums/update-album/:id', (req, res) => {
     const sql = "UPDATE albums SET title = ?, album_cover = ?, artist_id = ? WHERE id = ?";
 
     con.query(sql, [title, album_cover, artist_id, id], (err, result) => {
+        console.log(err);
         res.redirect('/albums');
     });
 });
@@ -118,7 +120,7 @@ router.post('/albums/delete-album/:id', (req, res) => {
 });
 
 // Likes
-router.get('/follows', (req, res) => {
+router.get('/likes', (req, res) => {
     const selected = req.query.select;
 
     const sql = "SELECT id, username FROM artists ORDER BY id"
@@ -126,18 +128,18 @@ router.get('/follows', (req, res) => {
     con.query(sql, (err, result) => {
         const options = result;
 
-        const sql = "SELECT a.id, a.username FROM artists a JOIN follows f ON a.id = f.followed_artist_id WHERE f.following_artist_id = ? AND f.unfollowed_at IS NULL ORDER BY a.id";
+        const sql = "SELECT a.id, a.title FROM albums a JOIN likes l ON a.id = l.liked_album_id WHERE l.liking_artist_id = ? AND l.unliked_at IS NULL ORDER BY a.id";
         
         con.query(sql, [selected], (err, result) => {
-            const following = result;
+            const liking = result;
 
-            const sql = "SELECT a.id, a.username FROM artists a LEFT JOIN follows f ON a.id = f.followed_artist_id AND f.following_artist_id = ? AND f.unfollowed_at is NULL WHERE f.id IS NULL AND a.id != ? ORDER BY a.id";
+            const sql = "SELECT a.id, a.username FROM artists a LEFT JOIN likes l ON a.id = l.liked_album_id AND l.liking_artist_id = ? AND l.unliked_at is NULL WHERE l.id IS NULL AND a.id != ? ORDER BY a.id";
 
             con.query(sql, [selected, selected], (err, result) => {
-                res.render('follows', {
+                res.render('Likes', {
                     id: selected,
                     options: options,
-                    following: following,
+                    liking: liking,
                     recommended: result
                 });
             });
@@ -146,28 +148,29 @@ router.get('/follows', (req, res) => {
 });
 
 // Like album
-router.post('/follows/follow/:following_id/:followed_id', (req, res) => {
-    const following_id = req.params.following_id;
-    const followed_id = req.params.followed_id;
+router.post('/likes/like/:liking_artist_id/:liked_album_id', (req, res) => {
+    const liking_id = req.params.liking_artist_id;
+    const liked_id = req.params.liked_album_id;
 
-    const sql = "INSERT INTO follows (followed_at, unfollowed_at, following_artist_id, followed_artist_id) VALUES (NOW(), NULL, ?, ?)"
+    const sql = "INSERT INTO likes (liked_at, unliked_at, liking_artist_id, liked_album_id) VALUES (NOW(), NULL, ?, ?)"
 
-    con.query(sql, [following_id, followed_id], (err, result) => {
-        res.redirect(`/follows/?select=${following_id}`);
+    con.query(sql, [liking_id, liked_id], (err, result) => {
+        res.redirect(`/likes/?select=${liking_id}`);
     });
 });
 
 // Unlike album
-router.post('/follows/unfollow/:following_id/:followed_id', (req, res) => {
-    const following_id = req.params.following_id;
-    const followed_id = req.params.followed_id;
+router.post('/likes/unlikes/:liking_artist_id/:liked_album_id', (req, res) => {
+    const liking_id = req.params.liking_artist_id;
+    const liked_id = req.params.liked_album_id;
 
-    const sql = "UPDATE follows SET unfollowed_at = NOW() WHERE following_artist_id = ? AND followed_artist_id = ? AND unfollowed_at IS NULL";
+    const sql = "UPDATE likes SET unliked_at = NOW() WHERE liking_artist_id = ? AND liked_artist_id = ? AND unliked_at IS NULL";
 
-    con.query(sql, [following_id, followed_id], (err, result) => {
-        res.redirect(`/follows/?select=${following_id}`);
+    con.query(sql, [liking_id, liked_id], (err, result) => {
+        res.redirect(`/likes/?select=${liking_id}`);
     });
 });
+
 
 module.exports = router;
 
