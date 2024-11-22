93ccbf6d5076a49afbf0e490a4e3225b48976402
Dawsxn <dawscatignas008@gmail.com>
2024-11-23 05:38:05 +0800

    feat: updated create track, update track, and read track

--- routes/tracks.js
+++ routes/tracks.js
@@ -24,29 +24,107 @@ router.get('/tracks', (req, res) => {
     });
 });
 
-// CREATE TRACKS
+// CREATE TRACKS (GET)
 
 router.get('/tracks/create', (req, res) => {
-    let genres;
-    let languages;
-    let albums;
-    con.query("SELECT * FROM genres", (err, result) => {genres = result});
-    con.query("SELECT * FROM languages", (err, result) => {languages = result});
-    con.query("SELECT * FROM albums", (err, result) => {albums = result});
-     res.render('tracks-form', {
-        action: "create",
-        disabled: false,
-        track: {
-            title: "",
-            audio: "",
-            duration: "",
-            explicit: "",
-        },
-        genres: genres,
-        languages: languages,
-        albums: albums
+    con.query("SELECT * FROM genres", (err, genres) => {
+        con.query("SELECT * FROM languages", (err, languages) => {
+            con.query("SELECT * FROM albums", (err, albums) => {
+                res.render('tracks-form', {
+                    action: "create",
+                    disabled: false,
+                    track: {
+                        title: "",
+                        audio: "",
+                        duration: "",
+                        explicit: "",
+                    },
+                    genres: genres,
+                    languages: languages,
+                    albums: albums,
+                });
+            });
+        });
     });
 });
+
+// CREATE TRACKS (POST)
+
+router.post('/tracks/create', (req, res) => {
+    const { title, audio, duration, explicit, genre, language, album } = req.body;
+
+    const sql = "INSERT INTO tracks (title, audio, duration, explicit, genre_id, language_id, album_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    con.query(sql, [title, audio, duration, explicit, genre, language, album], (err, result) => {
+        // console.log(err);
+        res.redirect('/tracks');
+    });
+});
+
+// READ TRACKS (GET)
+
+router.get('/tracks/read/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = "SELECT * FROM tracks WHERE id = ?";
+
+    con.query("SELECT * FROM genres", (err, genres) => {
+        con.query("SELECT * FROM languages", (err, languages) => {
+            con.query("SELECT * FROM albums", (err, albums) => {
+                con.query(sql, [id], (err, result) => {
+                    res.render('tracks-form', { 
+                        action: "read",
+                        disabled: true,
+                        track: result[0],
+                        genres: genres,
+                        languages: languages,
+                        albums: albums
+                    });
+                });
+            });
+        });    
+    });
+});
+
+// UPDATE TRACKS (GET)
+
+router.get('/tracks/update/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = "SELECT * FROM tracks WHERE id = ?";
+
+    con.query("SELECT * FROM genres", (err, genres) => {
+        con.query("SELECT * FROM languages", (err, languages) => {
+            con.query("SELECT * FROM albums", (err, albums) => {
+                con.query(sql, [id], (err, result) => {
+                    res.render('tracks-form', {
+                        action: `update/${id}`,
+                        disabled: false,
+                        track: result[0],
+                        genres: genres,
+                        languages: languages,
+                        albums: albums
+                    });
+                });
+            });
+        });        
+    });
+});
+
+// UPDATE TRACKS (POST)
+
+router.post('/tracks/update/:id', (req, res) => {
+    const { title, audio, duration, explicit, genre, language, album } = req.body;
+    const id = req.params.id;
+
+    const sql = "UPDATE tracks SET title = ?, audio = ?, duration = ?, explicit = ?, genre_id = ?, language_id = ?, album_id = ? WHERE id = ?";
+
+    con.query(sql, [title, audio, duration, explicit, genre, language, album, id], (err, result) => {
+        console.log(err);
+        res.redirect('/tracks');
+    });
+});
+
 
 // Export
 module.exports = router;
